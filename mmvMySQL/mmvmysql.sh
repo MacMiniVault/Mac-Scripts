20cd8435e4c038428cc6d0d2d60757062ec7a0c6
Jon Schwenn <jonschwenn@gmail.com>
2013-09-16 15:15:10 -0500

    checks for exisiting MySQL

--- mmvMySQL/mmvmysql.sh
+++ mmvMySQL/mmvmysql.sh
@@ -3,15 +3,31 @@
 # AUTHOR: JONATHAN SCHWENN @JONSCHWENN      #
 # MAC MINI VAULT - MAC MINI COLOCATION      #
 # MACMINIVAULT.COM - @MACMINIVAULT          #
-# VERSION 1.02 RELEASE DATE AUG 08 2013     #
+# VERSION 1.03 RELEASE DATE SEP 16 2013     #
 # DESC:  THIS SCRIPT INSTALLS MySQL on OSX  #
 #############################################
 #REQUIREMENTS:
 #  OS X 10.7 or newer
 #############################################
-#CHECK FOR OS X 10.7+
+# CHECK FOR OS X 10.7+
 if [[  $(sw_vers -productVersion | grep '10.[7-9]')  ]]
 then
+# CHECK FOR EXISTING MySQL
+if [[ -d /usr/local/mysql ]]
+then
+echo "It looks like you already have MySQL installed..."
+echo "This script will most likely fail unless MySQL is completley removed"
+echo "..."
+	while true; do
+		read -p "DO YOU WANT TO CONTINUE? [y/N]" yn
+		case $yn in
+		[Yy]* ) break;;
+		[Nn]* ) exit ;;
+		* ) echo "Please answer yes or no.";;
+		esac
+	done
+fi
+# LOOKS GOOD, LETS GRAB MySQL AND GET STARTED ...
 echo "Downloading MySQL Installers ... may take a few moments"
 curl -s -o ~/Downloads/MySQL.dmg http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.13-osx10.7-x86_64.dmg
 hdiutil attach -quiet ~/Downloads/MySQL.dmg
