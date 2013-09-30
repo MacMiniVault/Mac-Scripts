7b41e5383857430766378af8f849b1716f4be787
Jon Schwenn <jonschwenn@gmail.com>
2013-09-30 09:14:52 -0500

    updated for MySQL 5.6.14

--- mmvMySQL/mmvmysql.sh
+++ mmvMySQL/mmvmysql.sh
@@ -29,13 +29,13 @@ echo "..."
 fi
 # LOOKS GOOD, LETS GRAB MySQL AND GET STARTED ...
 echo "Downloading MySQL Installers ... may take a few moments"
-curl -s -o ~/Downloads/MySQL.dmg http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.13-osx10.7-x86_64.dmg
+curl -s -o ~/Downloads/MySQL.dmg http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.14-osx10.7-x86_64.dmg
 hdiutil attach -quiet ~/Downloads/MySQL.dmg
-cd /Volumes/mysql-5.6.13-osx10.7-x86_64/
+cd /Volumes/mysql-5.6.14-osx10.7-x86_64/
 echo "..."
 echo "..."
 echo "Installing MySQL, administrator password required ..."
-sudo installer -pkg mysql-5.6.13-osx10.7-x86_64.pkg -target /
+sudo installer -pkg mysql-5.6.14-osx10.7-x86_64.pkg -target /
 echo "..."
 echo "..."
 echo "Installing MySQL start up items..."
@@ -65,7 +65,7 @@ echo "Placing password on desktop..."
 echo "..."
 echo "..."
 cd ~/
-hdiutil detach -quiet /Volumes/mysql-5.6.13-osx10.7-x86_64/
+hdiutil detach -quiet /Volumes/mysql-5.6.14-osx10.7-x86_64/
 sleep 2
 rm ~/Downloads/MySQL.dmg
 # NEW MY.CNF PERFORMANCE OPTION START
