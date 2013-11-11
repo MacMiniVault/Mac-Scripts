2c73f1075db90c65b6bea7cb04fe4ceb521391c5
Jon Schwenn <jonschwenn@gmail.com>
2013-11-11 10:31:28 -0600

    fix if statement

--- unicastarp/unicastarp.sh
+++ unicastarp/unicastarp.sh
@@ -19,11 +19,10 @@ then
          echo "PATCH WAS PREVIOUSLY ENABLED"
          exit
       fi
+    fi
          sudo sysctl -w net.link.ether.inet.arp_unicast_lim=0  > /dev/null 2>&1
          echo "net.link.ether.inet.arp_unicast_lim=0" | sudo tee -a /etc/sysctl.conf  > /dev/null 2>&1
          sudo chown root:wheel /etc/sysctl.conf
          sudo chmod 644 /etc/sysctl.conf
-         echo "PATCH ENABLED"
-      
-   fi
+         echo "PATCH ENABLED" 
 fi
