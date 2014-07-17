c01cda4f744a0ef93898dfdd908ea05bfe32229d
Allen Hancock <hancock@yesthatallen.com>
2014-07-17 10:30:44 -0500

    specified versions

--- disablebonjour/disablebonjour.sh
+++ disablebonjour/disablebonjour.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# MAKES SURE WE ARE AT LEAST RUNNING 10.6 OR NEWER
+# MAKES SURE WE ARE RUNNING 10.6 -> 10.9 #YOSEMITE SUPPORT TBD
 if [[  $(sw_vers -productVersion | grep '10.[6-9]') ]]
 then
 # CHECKS FOR FLAG IN CURRENT PLIST FILE
 if [[ $(sudo /usr/libexec/PlistBuddy -c Print /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist | grep 'NoMulticast') ]]
 then
-echo "SORRY, MULTICAST IS ALREADY DISABLED"
+echo "MULTICAST DISABLED, NO CHANGES MADE"
 else
 sudo /usr/libexec/PlistBuddy -c "Add :ProgramArguments: string -NoMulticastAdvertisements" /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
-echo "PLEASE REBOOT"
+echo "MULTICAST DISABLED, PLEASE REBOOT"
 fi
 exit
 else
-echo "SORRY, THIS IS ONLY FOR OS X 10.6 OR NEWER"
+echo "THIS SCRIPT HAS BEEN TESTED ON MAC OS X 10.6 THROUGH OS X 10.9"
 fi
 exit
