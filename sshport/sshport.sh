447cff1fcd6e41b17662649c1e5f1db9f6fb3c79
ajleach <ajleach@gmail.com>
2016-02-05 09:16:43 -0600

    Remove ability to install on 10.11
    El Capitan's System Integrity Protection (SIP) does not allow this
    script to change the required files.

--- sshport/sshport.sh
+++ sshport/sshport.sh
@@ -7,10 +7,10 @@
 # DESC:  CHANGES THE PORT SSH LISTENS ON    #
 #############################################
 #REQUIREMENTS:
-#  OS X 10.8 or newer
+#  OS X 10.8, 10.9, 10.10
 #############################################
-#CHECK FOR OS X 10.8+
-if [[  $(sw_vers -productVersion | grep -E '10.[8-9]|1[0-0]')  ]]
+#CHECK FOR OS X 10.8-10.10
+if [[  $(sw_vers -productVersion | grep -E '10.[8-9]|10.10')  ]]
 then
 echo "THIS SCRIPT WILL CHANGE YOUR SSH PORT AND RESTART SSH."
 echo "THE DEFAULT PORT FOR SSH IS 22, WE RECOMMEND A CUSTOM"
@@ -21,11 +21,11 @@ echo "PLEASE ENTER A PORT NUMBER:"
 while read port; do
 	if [[ $port =~ ^[0-9]{2,5}$ ]]; then
 		if [[ $port = 22 ]]; then
-			sudo /usr/libexec/PlistBuddy -c "Set Sockets:Listeners:SockServiceName ssh" /System/Library/LaunchDaemons/ssh.plist 
+			sudo /usr/libexec/PlistBuddy -c "Set Sockets:Listeners:SockServiceName ssh" /System/Library/LaunchDaemons/ssh.plist
 echo "SYSTEM WILL NOW LISTEN ON DEFAULT PORT 22 FOR SSH"
-		break 2		
+		break 2
 		else
-			sudo /usr/libexec/PlistBuddy -c "Set Sockets:Listeners:SockServiceName $port" /System/Library/LaunchDaemons/ssh.plist 
+			sudo /usr/libexec/PlistBuddy -c "Set Sockets:Listeners:SockServiceName $port" /System/Library/LaunchDaemons/ssh.plist
 echo "SYSTEM WILL NOW LISTEN ON PORT $port FOR SSH"
 		break 2
 		fi
@@ -38,6 +38,6 @@ echo "yes" | sudo systemsetup -setremotelogin off > /dev/null 2>&1
 sleep 5
 sudo systemsetup -setremotelogin on > /dev/null 2>&1
 else
-echo "ERROR: YOU ARE NOT RUNNING OS X 10.8 OR NEWER"
+echo "ERROR: YOU ARE NOT RUNNING OS X 10.8, 10.9, or 10.10. THIS SCRIPT DOES NOT WORK ON 10.11: https://github.com/MacMiniVault/Mac-Scripts/blob/master/sshport/sshport-README.md"
 exit 1
 fi
