3a96e1b01b5095a62cbf13abaa1fe64c10e25dc7
Alex Leach <ajleach@gmail.com>
2019-05-09 11:10:20 -0500

    Script updated to version 4.0
    With the removal of a VPN server from macOS Server, this script now works in conjunction with VPN Enabler for Sierra/High Sierra/Mojave and no longer supports macOS versions before 10.12.

--- vpnscript/vpnscript.sh
+++ vpnscript/vpnscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #############################################
-# AUTHOR: JONATHAN SCHWENN @JONSCHWENN      #
-# MAC MINI VAULT - MAC MINI COLOCATION      #
+# ORIGINAL AUTHOR: JON SCHWENN @JONSCHWENN  #
+# CONTRIBUTOR: ALEX LEACH @AJLEACH          #
+# MAC MINI VAULT - MAC HOSTING              #
 # MACMINIVAULT.COM - @MACMINIVAULT          #
-# VERSION 3.00 RELEASE DATE NOV 11, 2015    #
+# VERSION 4.00 RELEASE DATE MAY 09, 2019    #
 # DESC:  THIS SCRIPT SETS UP A VPN SERVER   #
 #        THAT PLACES VPN CLIENTS IN A LOCAL #
 #        VLAN, ALLOWING CLIENTS TO ROUTE    #
@@ -11,130 +12,40 @@
 #        ONLY USING THE SINGLE PUBLIC IP    #
 #############################################
 #REQUIREMENTS:
-#  OS X 10.8, OS X 10.9.1 w/SERVER.APP 3.0.2, OS X 10.10 or OS X 10.11
-#  SERVER.APP INSTALLED / INITIALIZED
+#  MACOS 10.12 OR 10.13 OR 10.14
+#  VPN ENABLER FOR MOJAVE INSTALLED / CONFIGURED
+#  NO MACOS SERVER (SERVICES DISABLED / UNINSTALLED)
 #  NO VLANS CONFIGURED
-#  THIS SCRIPT WILL BACKUP AND REPLACE DNS AND FIREWALL CONFIGS
+#  THIS SCRIPT WILL BACKUP AND REPLACE FIREWALL CONFIGS
 #############################################
-#CHECK FOR OS X AND SERVER.app
+#CHECK FOR MACOS AND ENSURE SERVER.APP IS NOT INSTALLED
 OSX=no
-if [[  $(sw_vers -productVersion | grep '10.8') && $(serverinfo --configured | grep 'has') ]]
+if [[  $(sw_vers -productVersion | grep '10.12') && $(serverinfo --productname | grep 'not') ]]
 then
 OSX=yes
 fi
-if [[  $(sw_vers -productVersion | grep '10.9') && $(serverinfo --configured | grep 'has') && $(serverinfo --shortversion | grep -v '3.0.1') ]]
+if [[  $(sw_vers -productVersion | grep '10.13') && $(serverinfo --productname | grep 'not') ]]
 then
 OSX=yes
 fi
-if [[  $(sw_vers -productVersion | grep '10.10') && $(serverinfo --configured | grep 'has')  ]]
-then
-OSX=yes
-fi
-if [[  $(sw_vers -productVersion | grep '10.11') && $(serverinfo --configured | grep 'has')  ]]
+if [[  $(sw_vers -productVersion | grep '10.14') && $(serverinfo --productname | grep 'not') ]]
 then
 OSX=yes
 fi
 
 if [ $OSX = yes ]
 then
-echo "Congratulations, you are running OS X and have Server.app installed...."
+echo "Congratulations, you are running macOS and have do not have Server.app installed...."
 #CHECK IF SCRIPT HAS BEEN RUN BEFORE
 if [ -e /etc/vpn_MMV ]; then
-echo "SCRIPT CAN NOT BE RUN MORE THAN ONCE."
+echo "THIS VPN SCRIPT CANNOT BE RUN MORE THAN ONCE. INSTALLATION ABORTED. NO CHANGES HAVE BEEN MADE."
 exit 1
 else
 #CREATE TEST FILE TO ENSURE SCRIPT IS NOT EXECUTED MULTIPLE TIMES
 sudo touch /etc/vpn_MMV
-#STOP VPN AND DNS SERVICES
-sudo /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin stop dns > /dev/null 2>&1
-sudo /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin stop vpn > /dev/null 2>&1
 #START VLAN SETTINGS
 sudo networksetup -createVLAN LAN Ethernet 1
 sudo networksetup -setmanual LAN\ Configuration 10.0.0.1 255.255.255.0 10.0.0.1
-if [[ $(sw_vers -productVersion | grep '10.8') ]]
-then
-#FIND CURRENT RESOLVING DNS SERVERS
-RESOLVERS=$(grep nameserver /etc/resolv.conf | awk '{ printf ("%s;\n", $2) }')
-#START DNS SETTINGS
-#SETTING PERMS FOR EDITING - WILL SET PERMS BACK
-sudo chmod 666 /etc/named.conf
-#BACKUP AND RE-WRITE NAMED.CONF
-sudo cp /etc/named.conf /etc/named-backup.conf
-sudo cat << EOF > /etc/named.conf
-include "/etc/rndc.key";
-options {
-        directory "/var/named";
-        allow-recursion {
-                com.apple.ServerAdmin.DNS.public;
-        };
-        allow-transfer {
-                none;
-        };
-        forwarders {
-               $RESOLVERS
-        };
-};
-controls {
-        inet 127.0.0.1 port 54 allow {
-                "any";
-        } keys {
-                "rndc-key";
-        };
-};
-acl "com.apple.ServerAdmin.DNS.public" {
-        localhost;
-        localnets;
-};
-logging {
-        channel "_default_log" {
-                file "/Library/Logs/named.log";
-                severity info;
-                print-time yes;
-        };
-        category "default" {
-                "_default_log";
-        };
-};
-zone "." IN {
-        type hint;
-        file "named.ca";
-};
-zone "localhost" IN {
-        type master;
-        file "localhost.zone";
-        allow-update {
-                "none";
-        };
-};
-zone "0.0.127.in-addr.arpa" IN {
-        type master;
-        file "named.local";
-        allow-update {
-                "none";
-        };
-};
-EOF
-#SET PERMS BACK
-sudo chmod 644 /etc/named.conf
-#START FIREWALL SETTINGS
-#SETTING PERMS FOR EDITING - WILL SET PERMS BACK
-sudo chmod 666 /etc/pf.anchors/com.apple
-sudo cp /etc/pf.anchors/com.apple /etc/pf-backup
-sudo sed -i -e 's/^scrub-anchor "100.I/#scrub-anchor "100.I/' /etc/pf.anchors/com.apple
-sudo sed -i -e 's/^nat-anchor "100.I/#nat-anchor "100.I/' /etc/pf.anchors/com.apple
-sudo sed -i -e 's/^rdr-anchor "100.I/#rdr-anchor "100.I/' /etc/pf.anchors/com.apple
-sudo sed -i -e 's/^anchor "100.I/#anchor "100.I/' /etc/pf.anchors/com.apple
-#FIX APPLE TYPO FOR ADAPTIVE FIREWALL - APPLE KB TS4418
-sudo sed -i -e 's/^load anchor "400.AdaptiveFirewall\//load anchor "400.AdaptiveFirewall/' /etc/pf.anchors/com.apple
-sudo sed  -i -e '/^#anchor "100.I/  a\
-nat-anchor "100.customNATRules/*"\
-rdr-anchor "100.customNATRules/*"\
-load anchor "100.customNATRules" from "/etc/pf.anchors/customNATRules"
-'  /etc/pf.anchors/com.apple
-#SET PERMS BACK
-sudo chmod 644 /etc/pf.anchors/com.apple
-elif [[  $(sw_vers -productVersion | grep '[10.9|10.10|10.11]')   ]]
-then
 #START FIREWALL SETTINGS
 #SETTING PERMS FOR EDITING - WILL SET PERMS BACK
 sudo chmod 666 /etc/pf.anchors/com.apple
@@ -146,7 +57,6 @@ load anchor "100.customNATRules" from "/etc/pf.anchors/customNATRules"
 '  /etc/pf.anchors/com.apple
 #SET PERMS BACK
 sudo chmod 644 /etc/pf.anchors/com.apple
-fi
 #CREATE CUSTOM NAT RULES - SETTING PERMS FOR EDITING - WILL SET PERMS BACK
 sudo touch /etc/pf.anchors/customNATRules
 sudo chmod 666 /etc/pf.anchors/customNATRules
@@ -157,35 +67,21 @@ EOF
 #SET PERMS BACK
 sudo chmod 644 /etc/pf.anchors/customNATRules
 #ENABLE PF AND ENABLE KERNEL IP FORWARDING
-sudo /usr/libexec/PlistBuddy -c 'add :ProgramArguments:3 string -e' /System/Library/LaunchDaemons/com.apple.pfctl.plist
+sudo pfctl -f /etc/pf.conf > /dev/null 2>&1
 echo 'net.inet.ip.forwarding=1' | sudo tee -a /etc/sysctl.conf > /dev/null 2>&1
-#START VPN SETTINGS
-sudo /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings > /dev/null 2>&1 << EOF
-vpn:Servers:com.apple.ppp.l2tp:IPv4:DestAddressRanges:_array_index:0 = 10.0.0.150
-vpn:Servers:com.apple.ppp.l2tp:IPv4:DestAddressRanges:_array_index:1 = 10.0.0.200
-vpn:Servers:com.apple.ppp.l2tp:DNS:OfferedServerAddresses:_array_index:0 = 10.0.0.1
-EOF
-echo "ENTER VPN PASSPHRASE:"
-while read passphrase; do
-     if [[ (-z "${passphrase}") || ("${passphrase}" =~ ^[0-9]+$) ]]; then
-          echo "The passphrase you entered was empty or all numeric, please make it something a little more secure (long alphanumeric)..."
-          echo "ENTER VPN PASSPHRASE:"
-     else
-          echo "Checking strength of passphrase..."
-          break
-     fi
-done
-sudo /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings vpn:Servers:com.apple.ppp.l2tp:L2TP:IPSecSharedSecretValue = $passphrase
-sudo /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin start dns > /dev/null 2>&1
-sudo /Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin start vpn > /dev/null 2>&1
+#COPY AND CREATE PLIST
+sudo cp /System/Library/LaunchDaemons/com.apple.pfctl.plist /Library/LaunchDaemons/net.mmvpf.pfctl.plist
+sudo sed -i '' 's/com.apple.pfctl/net.mmvpf.pfctl/' /Library/LaunchDaemons/net.mmvpf.pfctl.plist
+sudo sed -i '' 's/>-f</>-e</' /Library/LaunchDaemons/net.mmvpf.pfctl.plist
+sudo sed -i '' '/pf\.conf/d' /Library/LaunchDaemons/net.mmvpf.pfctl.plist
+sudo launchctl load -w /Library/LaunchDaemons/net.mmvpf.pfctl.plist
 echo "VPN SETUP SUCCESSFUL"
 echo "REBOOT TO TAKE EFFECT"
-echo "ONCE REBOOTED, TURN VPN OFF AND BACK ON"
+echo "ONCE REBOOTED, MAKE SURE VPN ENABLER IS TURNED ON"
 fi
 exit 0
-#END IF STATEMENT CHECKING FOR OS X & SERVER.APP
+#END IF STATEMENT CHECKING FOR MACOS
 else
-echo "ERROR: YOU ARE NOT RUNNING OS X OR YOU DO NOT HAVE SERVER.APP INSTALLED"
-echo "NOTE: FOR MAVERICKS, SERVER.APP 3.0.2 OR HIGHER IS REQUIRED"
+echo "ERROR: YOU ARE NOT RUNNING MACOS 10.12/10.13/10.14 OR YOU HAVE SERVER.APP INSTALLED"
 exit 1
 fi
